c96ac9080d81e5e8203068ec18f3ae78436f3aa1
TheSoftNode <thesoftnode@gmail.com>
2024-02-08 09:45:19 +0100

    Add the nav bar, Ticket form and updated the main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,13 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
+import axios from "axios";
+import { BrowserRouter } from "react-router-dom";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
+axios.defaults.baseURL = "https://api.devempower.ir/TicketierAPI";
+
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+  <BrowserRouter>
     <App />
-  </React.StrictMode>,
-)
+  </BrowserRouter>
+);
